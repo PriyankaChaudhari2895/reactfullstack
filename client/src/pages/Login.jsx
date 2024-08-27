d55c1ee2566c3f54249fe17850562c8f879db6e6
Priyanka Chaudhari <36084903+PriyankaChaudhari2895@users.noreply.github.com>
2024-08-27 15:36:31 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../store/auth";
 import { toast } from "react-toastify";
-const URL  ="http://localhost:5000/api/auth/login";
+const URL  ="https://mern-stack-backend-4xqp.onrender.com";
 
 export const Login = () => {
   const [user, setUser] = useState({
