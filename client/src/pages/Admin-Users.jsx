ce82c6b72e64c50f326c2f497bcafb57dbe05952
Priyanka Chaudhari <36084903+PriyankaChaudhari2895@users.noreply.github.com>
2024-08-27 16:31:04 +0530

    Update Admin-Users.jsx

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -3,12 +3,12 @@ import { useAuth } from "../store/auth";
 import {Link} from "react-router-dom";
 export const AdminUsers = () => {
 
-const { authorizationToken } = useAuth();
+const { authorizationToken, API } = useAuth();
 const [users, setUsers] = useState([]);
 
  const getAllUsersData =  async()  => {
 try {
-    const response = await fetch("https://mern-stack-backend-4xqp.onrender.com", {
+    const response = await fetch(`${API}/api/admin/users`, {
         method:"GET",
         headers: {
             Authorization: authorizationToken ,
@@ -27,7 +27,7 @@ catch (error) {
 //  delete the user on delete button 
  const deleteUser = async (id) => {
  try{
-    const response = await fetch(`http://localhost:5000/api/admin/users/delete/${id}`, {
+    const response = await fetch(`${API}/api/admin/users/delete/${id}`, {
         method:"DELETE",
         headers: {
             Authorization: authorizationToken ,
